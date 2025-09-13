a6c9ea4f2d9426489180c9a60d0722a99e350b1c
bmalyj <159309870+bmalyj@users.noreply.github.com>
2025-09-13 10:57:29 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	char a1 = {};
+	int x = 0, y = 0, R = 0;
+	int random = 1+ rand() % 10 ;
+	cout << "paste int 1:";
+	cin >> x;
+	cout << "paste int 2:";
+	cin >> y;
+	cout << " знак (+,-,*,/,?, r) ";
+	cin >> a1;
+	switch (a1) {
+	case '+':
+		R = x + y;
+		cout << R;
+		break;
+	case '-':
+		R = x - y;
+		cout << R;
+		break;
+	case '*':
+		R = x * y;
+		cout << R;
+		break;
+	case '/':
+		if (y != 0) {
+			R = x / y;
+			cout << R;
+		}
+		else {
+			cout << "деление на ноль???";
+		}
+		break;
+	case '?':
+		while (x <= 100) {
+			cout << x << y << "think about it";
+		}
+	case 'r':
+		if (random == x or random == y) {
+			cout << "RIGHT!!!";
+		}
+		else {
+			cout << "Nah...";
+		}
+		cout << random;
+	
+	}
+	return 0;
+
+}
