6f2408811bbb2aa1c07f05a01e57d47765a6b3f7
bmalyj <159309870+bmalyj@users.noreply.github.com>
2025-10-18 11:48:30 +0300

    Update lab2-4.cpp

--- lab2-4.cpp
+++ lab2-4.cpp
@@ -1,55 +1,36 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
+void swapval(int a, int b) {
+    a = b;
+    b = a;
+    cout << "swap value a =" << a << "b " << b << endl;
+}
+
+void swappointer(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    cout << "swap pointer *a = " << *a << " *b  " << *b << endl;
+}
+
+void swaplink(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << " swwap link: a =" << a << ", b =" << b << endl;
+}
+
 int main() {
-    const int SIZE = 10;
-    int arr1[10];
-    int arr2[10];
+    int a = 5, b = 10;
+    cout << "start with " << a << " " << b;
+    swapval(a, b);
+    cout << "result: a = " << a << ", b = " << b << endl ;
 
-    int* p = arr1; 
+    swappointer(&a, &b);
+    cout << "result a = " << a << ", b = " << b << endl ;
 
-
-    *p = 0; 
-    *(p + 1) = 1;     
-
-    for (int i = 2; i < SIZE; i++) {
-        *(p + i) = *(p + i - 1) + *(p + i - 2);
-    }
-
-    cout << "array: ";
-    for (int i = 0; i < SIZE; i++) {
-        cout << *(p + i) << " ";
-    }
-    cout << endl;
-
-    int sum = 0;
-    int* q = arr1; 
-
-    for (int i = 0; i < SIZE; i++) {
-        sum += *(q + i);
-    }
-    cout << "sum :" << sum << endl;
-
-    int min = *arr1;
-    for (int i = 1; i < SIZE; i++) {
-        if (*(arr1 + i) < min) {
-            min = *(arr1 + i);
-        }
-    }
-    cout << "min:" << min << endl;
-
-    int* src = arr1;          
-    int* dest = arr2 + SIZE - 1; 
-    for (int i = 0; i < SIZE; i++) {
-        *dest = *src;
-        src++;
-        dest--;
-    }
-    cout << "2 array(obratno)";
-    for (int i = 0; i < SIZE; i++) {
-        cout << *(arr2 + i) << " ";
-    }
-    cout << endl;
-
+    swaplink(a, b);
+    cout << "result^ a = " << a << ", b = " << b << endl;
     return 0;
 }
